b6588597a4e15be808d224022f285486a6ad1bf0
g3spence <g3spence@889466c7-f7ff-4d08-8203-2c0ca75c1264>
2014-10-02 21:55:27 +0000

    Reformat documentation
    git-svn-id: https://markus.cdf.toronto.edu/svn/csc369-2014-09/group_0037@18 889466c7-f7ff-4d08-8203-2c0ca75c1264

--- a1/mymemory.c
+++ a1/mymemory.c
@@ -2,15 +2,17 @@
 
 #define SYSTEM_MALLOC 1
 
-/* mymalloc: allocates memory on the heap of the requested size. The block
-             of memory returned should always be padded so that it begins
-             and ends on a word boundary.
-     unsigned int size: the number of bytes to allocate.
-     retval: a pointer to the block of memory allocated or NULL if the
-             memory could not be allocated.
-             (NOTE: the system also sets errno, but we are not the system,
-                    so you are not required to do so.)
-*/
+/**
+ * Allocates memory on the heap of the requested size. The block
+ * of memory returned should always be padded so that it begins
+ * and ends on a word boundary.
+ *
+ * @param size the number of bytes to allocate.
+ * @return a pointer to the block of memory allocated or NULL if the
+ *         memory could not be allocated.
+ *         (NOTE: the system also sets errno, but we are not the system,
+ *         so you are not required to do so.)
+ */
 void *mymalloc(unsigned int size) {
 #if SYSTEM_MALLOC
     return malloc(size);
