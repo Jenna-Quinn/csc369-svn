795f4698045caf4fe5f496ad526cfa6661b28acb
g3spence <g3spence@889466c7-f7ff-4d08-8203-2c0ca75c1264>
2014-10-03 00:07:22 +0000

    Merge https://github.com/elliottsj/csc369-a1-seed
    Conflicts:
    	mymemory_opt.c
    
    git-svn-id: https://markus.cdf.toronto.edu/svn/csc369-2014-09/group_0037@20 889466c7-f7ff-4d08-8203-2c0ca75c1264

--- a1/mymemory_opt.c
+++ a1/mymemory_opt.c
@@ -3,16 +3,16 @@
 #define SYSTEM_MALLOC 1
 
 /**
-* Allocates memory on the heap of the requested size. The block
-* of memory returned should always be padded so that it begins
-* and ends on a word boundary.
-*
-* @param size the number of bytes to allocate.
-* @return a pointer to the block of memory allocated or NULL if the
-*         memory could not be allocated.
-*         (NOTE: the system also sets errno, but we are not the system,
-*         so you are not required to do so.)
-*/
+ * Allocates memory on the heap of the requested size. The block
+ * of memory returned should always be padded so that it begins
+ * and ends on a word boundary.
+ *
+ * @param size the number of bytes to allocate.
+ * @return a pointer to the block of memory allocated or NULL if the
+ *         memory could not be allocated.
+ *         (NOTE: the system also sets errno, but we are not the system,
+ *         so you are not required to do so.)
+ */
 void *mymalloc(unsigned int size) {
 #if SYSTEM_MALLOC
     return malloc(size);
@@ -21,12 +21,12 @@ void *mymalloc(unsigned int size) {
 }
 
 /**
-* unallocates memory that has been allocated with mymalloc.
-*
-* @param ptr pointer to the first byte of a block of memory allocated by mymalloc.
-* @return 0 if the memory was successfully freed and 1 otherwise.
-*         (NOTE: the system version of free returns no error.)
-*/
+ * unallocates memory that has been allocated with mymalloc.
+ *
+ * @param ptr pointer to the first byte of a block of memory allocated by mymalloc.
+ * @return 0 if the memory was successfully freed and 1 otherwise.
+ *         (NOTE: the system version of free returns no error.)
+ */
 unsigned int myfree(void *ptr) {
 #if SYSTEM_MALLOC
     free(ptr);
