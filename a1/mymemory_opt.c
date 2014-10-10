08714b3ebda28a3687cc706478f86ba046f6fba8
g3spence <g3spence@889466c7-f7ff-4d08-8203-2c0ca75c1264>
2014-10-10 21:39:46 +0000

    Implement next-fit strategy
    git-svn-id: https://markus.cdf.toronto.edu/svn/csc369-2014-09/group_0037@43 889466c7-f7ff-4d08-8203-2c0ca75c1264

--- a1/mymemory_opt.c
+++ a1/mymemory_opt.c
@@ -7,6 +7,18 @@
 #define MYMALLOCDEBUG 0
 
 /*
+ * Using uintptr_t to keep references to the heap start and end as integers.
+ * See <https://www.securecoding.cert.org/confluence/display/seccode/INT36-C.+Converting+a+pointer+to+integer+or+integer+to+pointer>
+ */
+static uintptr_t __heapstart = 0, __heapend = 0;
+
+// Pointer to the most recently-accessed block, for next-fit strategy
+static uintptr_t __last = 0;
+
+// Lock to ensure atomicity
+static pthread_mutex_t __lock = PTHREAD_MUTEX_INITIALIZER;
+
+/*
  * Structure to store block information inline with the allocated blocks.
  */
 struct __header_t {
@@ -27,18 +39,6 @@ struct __header_t {
 };
 
 #define MAGIC 1234
-
-/*
- * Using uintptr_t to keep references to the heap start and end as integers.
- * See <https://www.securecoding.cert.org/confluence/display/seccode/INT36-C.+Converting+a+pointer+to+integer+or+integer+to+pointer>
- */
-static uintptr_t __heapstart = 0, __heapend = 0;
-
-// Pointer to the most recently-accessed free block, for next-fit strategy
-static struct __header_t __last;
-
-// Lock to ensure atomicity
-static pthread_mutex_t __lock = PTHREAD_MUTEX_INITIALIZER;
 
 /**
 * Verify block integrity by checking the magic number
@@ -80,7 +80,7 @@ static void __dump_heap(void) {
 * Initialize heap pointers. Return -1 on error.
 */
 static int __init(void) {
-    __heapstart = __heapend = (uintptr_t) sbrk(0);
+    __heapstart = (uintptr_t) sbrk(0);
     if (__heapstart == -1) {
 #if MYMALLOCDEBUG
             warn("Initial sbrk failed");
@@ -101,8 +101,9 @@ static int __init(void) {
         }
 
         __heapstart += x;
-        __heapend = __heapstart;
     }
+
+    __heapend = __heapstart;
 
     return 0;
 }
@@ -127,8 +128,8 @@ static void __split_block(struct __header_t *h, size_t size) {
     new_h->prev = h;
     new_h->next = h->next;
     new_h->size = h->size - sizeof(struct __header_t) - size;
-    new_h->in_use = 0;
     new_h->magic = MAGIC;
+    new_h->in_use = 0;
 
     if (h->next != (struct __header_t *) __heapend)
         h->next->prev = new_h;
@@ -183,6 +184,79 @@ static void *__extend_heap(size_t incr) {
 }
 
 /**
+ * Attempt to use the given block to allocate the given number of bytes.
+ * 
+ * @param h    a pointer to the block to attempt to allocate
+ * @param size the number of bytes to allocate
+ * @return     a pointer to the newly-allocated block, or NULL if the block could not be allocated
+ */
+static struct __header_t *__try_allocate_block(struct __header_t *h, unsigned int size) {
+    /* Verify block integrity */
+    __check_magic_number(h);
+
+    if (h->in_use) {
+        // Block is in use
+        return NULL;
+    }
+
+    if (h->size < size) {
+        // Block is too small
+        return NULL;
+    }
+
+    /* Valid free block found */
+
+#if MYMALLOCDEBUG
+        warnx("HEAP BEFORE SPLIT:");
+        __dump_heap();
+#endif
+
+    /* Attempt to split the block */
+    __split_block(h, size);
+
+#if MYMALLOCDEBUG
+        warnx("HEAP AFTER SPLIT:");
+        __dump_heap();
+#endif
+
+    /* Flag block as in-use */
+    h->in_use = 1;
+    
+    return h;
+};
+
+/**
+ * Attempt to find and allocate a block in a sequence of blocks from start to end
+ *
+ * @param start  memory address of a block at the beginning of the sequence
+ * @param end    memory address after the last block in the sequence
+ * @param prev_h pointer to a (struct __header_t *); will point at the block *before* the successfully-allocated block,
+ *               or at the last block in the sequence if no block was allocated. prev_h is not modified if the *first*
+ *               block in the sequence was allocated.
+ * @return       a pointer to the newly-allocated block, or NULL if the block could not be allocated
+ */
+static struct __header_t *__find_allocate_block(uintptr_t start, uintptr_t end, unsigned int size, struct __header_t **prev_h) {
+    struct __header_t *h, *_prev_h = NULL;
+
+    uintptr_t current;
+    for (current = start; current < end; current = (uintptr_t) h->next) {
+        h = (struct __header_t *) current;
+        if (__try_allocate_block(h, size) != NULL) {
+            /* Successfully allocated block */
+            if (_prev_h != NULL)
+                *prev_h = _prev_h;
+            return h;
+        }
+        _prev_h = h;
+    }
+
+    /* No block could be allocated */
+    if (_prev_h != NULL)
+        *prev_h = _prev_h;
+    return NULL;
+};
+
+/**
 * Allocates memory on the heap of the requested size. The block
 * of memory returned should always be padded so that it begins
 * and ends on a word boundary.
@@ -200,7 +274,7 @@ void *mymalloc(unsigned int size) {
 
     struct __header_t
             *new_h = NULL,  // Block header for the newly-allocated block
-            *curr_h = NULL; // Current header when iterating over block list
+            *prev_h = NULL; // Current header when iterating over block list
 
     /* Lock to prevent concurrent access */
     pthread_mutex_lock(&__lock);
@@ -213,52 +287,25 @@ void *mymalloc(unsigned int size) {
     /*
      * Next-fit strategy to find free regions of memory
      */
-    uintptr_t i;
-    for (i = __heapstart; i < __heapend; i += sizeof(struct __header_t) + curr_h->size) {
-        curr_h = (struct __header_t *) i;
-
-        /* Verify block integrity */
-        __check_magic_number(curr_h);
-
-        if (curr_h->in_use) {
-            // Block is in use
-            continue;
-        }
-
-        if (curr_h->size < size) {
-            // Block is too small
-            continue;
-        }
-
-        /* Valid free block found */
-        new_h = curr_h;
-
-#if MYMALLOCDEBUG
-        warnx("HEAP BEFORE SPLIT:");
-        __dump_heap();
-#endif
-
-        /* Attempt to split the block */
-        __split_block(new_h, size);
-
-#if MYMALLOCDEBUG
-        warnx("HEAP AFTER SPLIT:");
-        __dump_heap();
-#endif
-
-        /* Flag block as in-use */
-        new_h->in_use = 1;
-    }
+    if (__last != 0)
+        /* Start at the most-recently accessed block */
+        new_h = __find_allocate_block(__last, __heapend, size, &prev_h);
+    if (new_h == NULL)
+        /* Have not allocated block yet; iterate from the beginning of the heap */
+        new_h = __find_allocate_block(__heapstart, __last != 0 ? __last : __heapend, size, &prev_h);
 
     if (new_h == NULL) {
         /* No valid free block found; extend the heap with sbrk */
         new_h = __extend_heap(sizeof(struct __header_t) + size);
-        new_h->prev = curr_h;
+        new_h->prev = prev_h;
         new_h->next = (struct __header_t *) __heapend;
         new_h->size = size;
-        new_h->in_use = 1;
         new_h->magic = MAGIC;
+        new_h->in_use = 1;
     }
+
+    /* new_h is the most-recently accessed block */
+    __last = (uintptr_t) new_h;
 
 #if MYMALLOCDEBUG
     __dump_heap();
