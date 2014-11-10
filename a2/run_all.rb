3eaaf80b058a5429cd9c58c1d94ea46b4ddf5d1e
g3spence <g3spence@889466c7-f7ff-4d08-8203-2c0ca75c1264>
2014-11-10 22:52:36 +0000

    Add initial run_all script
    git-svn-id: https://markus.cdf.toronto.edu/svn/csc369-2014-09/group_0037@73 889466c7-f7ff-4d08-8203-2c0ca75c1264

---
+++ a2/run_all.rb
@@ -0,0 +1,16 @@
+# run_all.rb
+#
+# Run ./sim for all four page replacement algorithms and output a table for each execution
+# Usage:
+#   ruby run_all.rb
+
+TRACES = [
+    '/u/csc369h/fall/pub/a2-traces/simpleloop',
+    # '/u/csc369h/fall/pub/a2-traces/matmul-100',
+    # '/u/csc369h/fall/pub/a2-traces/blocked-100-25',
+    # '<(valgrind --tool=lackey --trace-mem=yes make)'
+]
+
+TRACES.each do |trace|
+  `./sim -f #{trace} -m 150 -a fifo`
+end
