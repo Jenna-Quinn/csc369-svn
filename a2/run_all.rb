534d41d5efeae14fd153ae2cc0cdb48415e8c00b
g3spence <g3spence@889466c7-f7ff-4d08-8203-2c0ca75c1264>
2014-11-11 00:20:35 +0000

    Remove progress bar, create make.trace
    git-svn-id: https://markus.cdf.toronto.edu/svn/csc369-2014-09/group_0037@76 889466c7-f7ff-4d08-8203-2c0ca75c1264

--- a2/run_all.rb
+++ a2/run_all.rb
@@ -1,16 +1,25 @@
 # run_all.rb
 #
-# Run ./sim for all four page replacement algorithms and output a table for each execution
+# Run ./sim for all four page replacement algorithms and output a table for each execution to a file tables.txt
 # Usage:
 #   ruby run_all.rb
 
 TRACES = [
-    '/u/csc369h/fall/pub/a2-traces/simpleloop',
-    # '/u/csc369h/fall/pub/a2-traces/matmul-100',
-    # '/u/csc369h/fall/pub/a2-traces/blocked-100-25',
-    # '<(valgrind --tool=lackey --trace-mem=yes make 2>&1)'
+  '/u/csc369h/fall/pub/a2-traces/simpleloop',
+  # '/u/csc369h/fall/pub/a2-traces/matmul-100',
+  # '/u/csc369h/fall/pub/a2-traces/blocked-100-25',
+  './make.trace'
 ]
 
+# Use valgrind to create make.trace
+puts 'Creating \'make.trace\'...'
+unless system 'valgrind --tool=lackey --trace-mem=yes make > make.trace 2>&1'
+  puts 'Failed to create \'make.trace\''
+  exit!
+end
+
 TRACES.each do |trace|
-  `./sim -f #{trace} -m 150 -a fifo`
+  command = "cat #{trace} | ./sim -m 50 -a fifo"
+  puts "Running command: #{command}\n"
+  puts `#{command}`
 end
